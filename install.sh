601741efd6d4ccc4dac773dbc4c60278143ec477
shaoying <shylinux@icloud.com>
2018-03-20 00:00:35 +0800

    mod go

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-sudo tar xvf go1.6.2.linux-386 -C /usr/local
+[ -e /usr/local/go ] || sudo tar xvf go1.6.2.linux-386 -C /usr/local
 tar xvf golang.tar.gz -C ~
 tar xvf vim.tar.gz -C ~
 
-export GOPATH=~/go
+export GOPATH=~/context:~/go
 export GOBIN=~/go/bin
 export PATH=$PATH:~/go/bin
+export PATH=$PATH:/usr/local/go/bin
 
-go install ~/go/src/github.com/nsf/gocode/
-go install ~/go/src/github.com/jstemmer/gotags/
-go install ~/go/src/github.com/skip2/go-qrcode/
-go install ~/go/src/github.com/go-sql-driver/mysql/
+cd ~/go/src/github.com/
+go install ./nsf/gocode/
+go install ./jstemmer/gotags/
+go install ./skip2/go-qrcode/
+go install ./go-sql-driver/mysql/
 
